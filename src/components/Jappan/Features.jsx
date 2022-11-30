5616cb487915e0e22aeb6b36ef03d5e9c251f769
febkosq8 <febkosq8@yahoo.com>
2022-11-30 23:20:48 +0000

    Jappan Css changes

--- src/components/Jappan/Features.jsx
+++ src/components/Jappan/Features.jsx
@@ -19,53 +19,56 @@ export default function Features() {
     {
       title: " Music Playback",
       icon: <FontAwesomeIcon className="ms-2" icon={faMusic} />,
-      img:[]
-      // img1: MusicPlay1,
-      // img1Title: "Play your favourite playlist",
-      // img1Width: "30vh",
-      // img1Ratio: 2.3,
-      // img2: "",
-      // img2Title: "",
-      // img2Width: "30vh",
-      // img2Ratio: 2.3,
-      // img3: "",
-      // img3Title: "",
-      // img3Width: "30vh",
-      // img3Ratio: 2.3,
+      img: [
+        {
+          img: MusicPlay1,
+          title: "Play your favourite playlist",
+          width: "20vw",
+          ratio: "auto",
+        },
+      ],
     },
     {
       title: " Leveling System",
       icon: <FontAwesomeIcon className="ms-2" icon={faPersonArrowUpFromLine} />,
-      img:[
+      img: [
         {
-          img:Level1,
-          title:"Know who all are active in a server",
-          ratio:"auto",
-          width:"20vw"
-        },{
-          img:Level2,
-          title:"Leveling up as you interact",
-          width:"20vw",
-          ratio:"auto"
-        }
-      ]
+          img: Level1,
+          title: "Know who all are active in a server",
+          ratio: "auto",
+          width: "20vw",
+        },
+        {
+          img: Level2,
+          title: "Leveling up as you interact",
+          width: "20vw",
+          ratio: "auto",
+        },
+      ],
     },
     {
       title: " Moderation Tools",
       icon: <FontAwesomeIcon className="ms-2" icon={faGavel} />,
-      img:[]
-      // img1: Mod1,
-      // img1Title: "Audit Events",
-      // img1Width: "30vh",
-      // img1Ratio: 2.3,
-      // img2: Mod2,
-      // img2Title: "Clear Messages",
-      // img2Width: "30vh",
-      // img2Ratio: 2.3,
-      // img3: Mod3,
-      // img3Title: "Member I-O Announcements",
-      // img3Width: "30vh",
-      // img3Ratio: 2.3,
+      img: [
+        {
+          img: Mod1,
+          title: "Audit Events",
+          ratio: "auto",
+          width: "20vw",
+        },
+        {
+          img: Mod2,
+          title: "Clear Messages",
+          width: "20vw",
+          ratio: "auto",
+        },
+        {
+          img: Mod3,
+          title: "Member I-O Announcements",
+          width: "20vw",
+          ratio: "auto",
+        },
+      ],
     },
   ];
   return (
@@ -78,14 +81,17 @@ export default function Features() {
         return (
           <div className="card text-bg-dark m-1 p-2 d-flex flex-column align-items-center justify-content-center col-md-6 col-sm-8">
             <h3 className="card-title">
-                {item.icon}
-                {item.title}
-              </h3>
+              {item.icon}
+              {item.title}
+            </h3>
             <div className="d-flex flex-column align-items-center card-body">
-              <div
-                className=" gap-2 d-flex flex-row flex-wrap align-items-center justify-content-center"
-              >
-                {item.img.map((img,index)=> <div className={`d-flex flex-row align-items-center w-100 justify-content-between ${index!==item.img.length-1&&"border-bottom"}  py-4`}>
+              <div className="gap-2 d-flex flex-row flex-wrap align-items-center justify-content-center">
+                {item.img.map((img, index) => (
+                  <div
+                    className={`d-flex flex-row align-items-center w-100 justify-content-between ${
+                      index !== item.img.length - 1 && "border-bottom"
+                    }  py-4`}
+                  >
                     <h5 className="fw-light">{img.title}</h5>
                     <img
                       src={img.img}
@@ -96,7 +102,8 @@ export default function Features() {
                       }}
                       alt={img.title}
                     />
-                  </div>)}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
--- src/components/Jappan/Header.jsx
+++ src/components/Jappan/Header.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 export default function Header() {
   return (
-    <div className="header">
-      <nav id="navbar-jappan" className="navbar fixed-top bg-light py-2">
-        <a className="navbar-brand px-2" href="/Jappan">
-          Jappan
-        </a>
-        <ul className="nav nav-pills">
-          <li className="nav-item">
-            <a className="nav-link" href="#about">
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#features">
-              Features
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#team">
-              Team
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav id="navbar-jappan" className="navbar fixed-top bg-light py-2">
+      <a className="navbar-brand px-2" href="/Jappan">
+        Jappan
+      </a>
+      <ul className="nav nav-pills">
+        <li className="nav-item">
+          <a className="nav-link" href="#about">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#features">
+            Features
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#team">
+            Team
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
